63d50f7fc03505f93c56ea892842933ef6bda310
Eric Brooke <ericbrooke@mac.com>
2014-12-03 14:16:25 -0800

    add boostrap and angular-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
+//= require angular
+//= require angular-ui-router
 //= require_tree .
