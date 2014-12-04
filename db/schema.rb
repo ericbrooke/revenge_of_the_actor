f290b6bcde4f6b97e9a23f72cbe4993f453ba972
Eric Brooke <ericbrooke@mac.com>
2014-12-04 11:41:11 -0800

    migrated authorisation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201224911) do
+ActiveRecord::Schema.define(version: 20141204193450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authorizations", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "user_id"
+    t.string   "token"
+    t.string   "secret"
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
