7d689436055428845e4b6571245dd03af880bfd7
Eric Brooke <ericbrooke@mac.com>
2014-12-04 16:26:52 -0800

    debug ominiauth, routes and added shares model contolerr

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :authorizations, dependent: :destroy, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  has_many :authorizations, dependent: :destroy
 end
---
+++ spec/factories/shares.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :share do
+    from_user_id 1
+to_user_id 1
+to_email "MyString"
+created_at "2014-12-04 16:26:06"
+url "MyString"
+  end
+
+end
