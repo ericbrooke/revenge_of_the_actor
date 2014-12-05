dc75516781caf7f60e590f3f4f4123bb83c61dd2
Eric Brooke <ericbrooke@mac.com>
2014-12-05 09:57:35 -0800

    removed simplecov, put back when tests can be run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-SimpleCov.start
 
 require 'rspec/rails'
 require 'capybara/rails'
