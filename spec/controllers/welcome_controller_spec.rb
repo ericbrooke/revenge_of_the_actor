1af0e71e4037d0c31259f09ac018c29cd20ab9e5
Eric Brooke <ericbrooke@mac.com>
2014-12-04 17:05:45 -0800

    added tests bug in there.  Added welcome and blank pages

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
 
+  describe "GET #index" do
+    before :each do
+      @user = User.new
+    end
+
+    it "should render the index view without a user" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "should render the dashboard view with the angular template" do
+      sign_in @user
+      get :dashboard
+      expect(response).to render_template :dashboard
+      expect(response).to render_template layout: "layouts/angular"
+    end
+
+    it "should redirect from dashboard view to index without a user" do
+      get :dashboard
+      expect(response).to redirect_to root_url
+    end
+  end
 end
