74cf00458d12b0c331021cc2e6bb0819ba6b2f26
Eric Brooke <ericbrooke@mac.com>
2014-12-04 17:24:56 -0800

    cleaned up messes and added sessions controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require angular
-//= require angular-ui-router
 //= require_tree .
